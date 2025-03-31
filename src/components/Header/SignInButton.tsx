5d7cd8f47e10a8fdcda195c02eb82d873dc338fd
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-03-31 13:25:20 +0800

    feat: link the gcashflow app and homepage

--- src/components/Header/SignInButton.tsx
+++ src/components/Header/SignInButton.tsx
@@ -8,7 +8,7 @@ export const SignInButton: React.FC<SignInButtonProps> = ({
   fullWidth = false,
 }) => {
   return (
-    <Link href="/signin">
+    <Link href={`${process.env.NEXT_PUBLIC_DEMO_APP_URL}`}>
       <button
         className={`bg-gradient-to-r from-blue-600 to-blue-400 hover:from-blue-500 hover:to-blue-300 
           text-white font-bold py-3 px-8 rounded-full transition-all duration-300 transform hover:scale-105 cursor-pointer
